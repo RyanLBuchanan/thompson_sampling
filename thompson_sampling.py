a1870f89b975a84523ae7c01bb2a308fd4ea3d78
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-16 09:39:25 -0600

    Import libraries

--- thompson_sampling.py
+++ thompson_sampling.py
@@ -1,1 +1,7 @@
-# Thompson Sampling Algorithm tutorial from Machine Learning A-Z - SuperDataScience -> Input by Ryan L Buchanan 15OCT20
+# Thompson Sampling Algorithm tutorial from Machine Learning A-Z - SuperDataScience -> Input by Ryan L Buchanan 15OCT 20
+
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
