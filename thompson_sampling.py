c51e2728852b266a80dfe978f106aa77ec69617a
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-16 12:03:21 -0600

    Thompson Sampling model complete

--- thompson_sampling.py
+++ thompson_sampling.py
@@ -17,7 +17,7 @@ d = 10
 ads_selected = []
 numbers_of_rewards_1 = [0] * d
 numbers_of_rewards_0 = [0] * d
-
+total_reward = 0
 for n in range(0, N):
     ad = 0
     max_random = 0
@@ -26,7 +26,14 @@ for n in range(0, N):
         if (random_beta > max_random):
             max_random = random_beta
             ad = i
-
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    if reward == 1:
+        numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
+    else:
+        numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
+    total_reward = total_reward + reward
+    
 # Visualize the results in histogram
 plt.hist(ads_selected)
 plt.title('Histogram of Ads Selections')
